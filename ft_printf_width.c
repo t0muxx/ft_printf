531a8527f4caa48cbee3bcad96cdb018238696ef
Tom MARAVAL <tmaraval@e1r13p4.42.fr>
2018-01-16 11:14:00 +0100

    gestion flag 0

--- ft_printf_width.c
+++ ft_printf_width.c
@@ -6,7 +6,7 @@
 /*   By: tomlulu <tomlulu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/14 14:39:10 by tomlulu           #+#    #+#             */
-/*   Updated: 2018/01/16 11:06:56 by tmaraval         ###   ########.fr       */
+/*   Updated: 2018/01/16 11:13:21 by tmaraval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	ft_printf_width(t_parsed_opt *opt)
 			opt->str_arg = ft_strjoin(opt->str_arg, " ");
 			free(temp);
 		}
-		else if (opt->bin_flag & FLG_ZERO)
+		else if (opt->bin_flag & FLG_ZERO && opt->in_precision == -1)
 		{
 			opt->in_precision = opt->in_width;
 			opt->in_width = 0;
